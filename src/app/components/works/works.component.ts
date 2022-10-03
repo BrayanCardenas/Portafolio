9b5edb99ce9903405e76949416085abb21db5506
BrayanCardenas <brayansti34@gmail.com>
2022-10-03 16:29:19 -0500

    TReducción del tamaño de las IMG

--- src/app/components/works/works.component.ts
+++ src/app/components/works/works.component.ts
@@ -10,61 +10,38 @@ export class WorksComponent implements OnInit {
   works = [
     {
       name: 'Mundano',
-      img: '../../../assets/img/works/proyecto-mundano.png',
+      img: '../../../assets/img/works/mundano.jpg',
       description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
       tecnologia: [ 'HTML', 'CSS', 'JavaScript'],
-      url: 'https://mundano.netlify.app/'
-
+      url: 'https://brayancardenas.github.io/mundano/'
     },
     {
       name: 'Cafe',
-      img: '../../../assets/img/works/proyecto-cafe.png',
+      img: '../../../assets/img/works/cafe.jpg',
       description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
       tecnologia: [ 'HTML', 'Sass', 'JavaScript'],
-      url: 'https://cafebc.netlify.app'
+      url: 'https://brayancardenas.github.io/Cafe/'
     },
     {
       name: 'Restaurante',
-      img: '../../../assets/img/works/proyecto-restaurante.png',
+      img: '../../../assets/img/works/weiddgan.png',
       description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
       tecnologia: [ 'HTML', 'CSS', 'Bootstrap','JavaScript'],
-      url: 'https://restaurantebc.netlify.app'
+      url: 'https://brayancardenas.github.io/Weiddgan'
     },
     {
-      name: 'Brahama',
-      img: '../../../assets/img/works/Proyecto2.png',
+      name: 'Agenda',
+      img: '../../../assets/img/works/agenda.jpg',
       description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
-      tecnologia: [ 'HTML', 'CSS', 'JavaScript']
+      tecnologia: [ 'HTML', 'CSS', 'JavaScript'],
+      url: 'https://brayancardenas.github.io/Agenda/'
     },
     {
-      name: 'Mundano',
-      img: '../../../assets/img/works/Proyecto3.png',
+      name: 'Carrito',
+      img: '../../../assets/img/works/carrito-compras.jpg',
       description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
-      tecnologia: [ 'HTML', 'CSS', 'JavaScript']
-    },
-    {
-      name: 'Brahama',
-      img: '../../../assets/img/works/proyecto-mundano.png',
-      description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
-      tecnologia: [ 'HTML', 'CSS', 'JavaScript']
-    },
-    {
-      name: 'Brahama',
-      img: '../../../assets/img/works/proyecto-cafe.png',
-      description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
-      tecnologia: [ 'HTML', 'CSS', 'JavaScript']
-    },
-    {
-      name: 'Mundano',
-      img: '../../../assets/img/works/proyecto-restaurante.png',
-      description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
-      tecnologia: [ 'HTML', 'CSS', 'JavaScript']
-    },
-    {
-      name: 'Brahama',
-      img: '../../../assets/img/works/Proyecto2.png',
-      description: 'Aqui va una descripción del proyecto esto es para rellenar el campo vacio que hay para que se veamas organico y compleo al finalizar el mockup',
-      tecnologia: [ 'HTML', 'CSS', 'JavaScript']
+      tecnologia: [ 'HTML', 'CSS', 'JavaScript'],
+      url: 'https://brayancardenas.github.io/Carrito_Compras/'
     },
   ]
 
